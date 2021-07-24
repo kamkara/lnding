16b1e800163e674b429bfd2885433280b4abeb0f
kamkara <lnclass.kamkara@gmail.com>
2021-07-24 13:09:58 +0000

    app: add admin sign in/up page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,28 @@
 module ApplicationHelper
     def title
-    base_title = "Motiv'Action"
-    if @title.nil?
-       base_title
-    else
-     " #{@title} - #{base_title}"
+        base_title = "Motiv'Action"
+        if @title.nil?
+        base_title
+        else
+        " #{@title} - #{base_title}"
+        end
     end
-end
+
+    #Sign in and up anywhere on app
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
 
