3eea0d5b37c6ef8dedc5156dfa777ffe7fdee708
kamkara <lnclass.kamkara@gmail.com>
2021-07-25 13:48:54 +0000

    oui

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :resultats
   get 'admin/index'
   devise_for :users
-  root to:'home#index'
+  root to:'resultats#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
